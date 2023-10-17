23d1646bea81c407be4aff8bbeb4ef75c96e4c83
Armen Yavryan <armenyavryan1992@gmail.com>
2023-10-17 11:31:52 +0400

    added first todo html

---
+++ src/App.svelte
@@ -0,0 +1,43 @@
+<script></script>
+
+<main>
+    <div class="todo__wrapper">
+        <h1 class="todo__title">Your Todo List</h1>
+        <button>Add ToDo</button>
+            <div class="card__wrapper">
+                <div class="todo__cardItem">
+                        <p class="todo__cardDescription"></p>
+                    <button>Delete todo</button>
+                </div>
+            </div>
+    </div>
+</main>
+
+<style>
+    body {
+        padding: 0;
+        margin: 0;
+    }
+
+    .todo__wrapper {
+        width: 100%;
+        height: 100%;
+        background: #e4b45c;
+    }
+
+    .todo__title {
+        text-align: center;
+    }
+
+    .card__wrapper {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+
+    .todo__cardItem {
+        padding: 30px;
+        border: 1px solid green;
+        border-radius: 4px;
+    }
+</style>
