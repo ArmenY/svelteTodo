8689c59647ee00bca3280dac3e230b33f055f2df
Armen Yavryan <armenyavryan1992@gmail.com>
2023-10-21 00:03:48 +0400

    minified style of card component

--- src/Card.svelte
+++ src/Card.svelte
@@ -5,7 +5,6 @@
         console.log(todoItems)
     }
 </script>
-
 <div class="card__wrapper">
     {#each todoItems as todoItem (todoItem.id)}
 
@@ -25,49 +24,6 @@
     {/each}
 
 </div>
-
 <style>
-    .card__wrapper {
-        display: flex;
-        align-items: center;
-        justify-content: flex-start;
-        flex-wrap: wrap;
-    }
-
-    .card__deleteBtn {
-        align-self: flex-end;
-    }
-
-    .todo__cardItem {
-        width: 100%;
-        max-width: 250px;
-        margin: 20px;
-        padding: 30px;
-        border: 1px solid #008000;
-        border-radius: 4px;
-    }
-
-    .todo__title {
-        font-family: sans-serif;
-        font-size: 20px;
-        font-weight: 700;
-        line-height: 1.5;
-    }
-
-    .todo__cardDescription {
-        font-family: sans-serif;
-        font-size: 16px;
-        font-weight: 500;
-        line-height: 1.5;
-    }
-
-    .card__deleteBtn {
-        margin-top: 25px;
-        margin-right: 0;
-        font-family: sans-serif;
-        font-size: 16px;
-        font-weight: 600;
-        color: #fff;
-        background: #e4b45c;
-    }
+    .card__wrapper {display: flex;align-items: center;justify-content: flex-start;flex-wrap: wrap;}  .card__deleteBtn {align-self: flex-end;}  .todo__cardItem {width: 100%;max-width: 250px;margin: 20px;padding: 30px;border: 1px solid #008000;border-radius: 4px;}  .todo__title {font-family: sans-serif;font-size: 20px;font-weight: 700;line-height: 1.5;}  .todo__cardDescription {font-family: sans-serif;font-size: 16px;font-weight: 500;line-height: 1.5;}  .card__deleteBtn {margin-top: 25px;margin-right: 0;font-family: sans-serif;font-size: 16px;font-weight: 600;color: #fff;background: #e4b45c;}
 </style>
